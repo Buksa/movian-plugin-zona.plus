378229065607ae81bd3ab104e8ad893cb2ff79fc
Buksa <buksa.noob@gmail.com>
2022-12-24 20:25:41 -0800

    добавил пример страницы list пока без лоадера

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 //загружаем module page https://github.com/andoma/movian/blob/master/res/ecmascript/modules/movian/page.js
 var page = require('movian/page');
+//загружаем module http https://github.com/andoma/movian/blob/master/res/ecmascript/modules/movian/http.js
+//нужен для запросов http
+var http = require('movian/http');
+//нужен для работы с елементами DOM
+var html = require('movian/html');
 //каждый плагин должен быть с уникальным id который мы указываем в URI
 var PREFIX = 'zona.plus';
+var BASE_URL = 'https://w139.zona.plus';
+var UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36';
 
 // api 1 https://movian.tv/projects/movian/wiki/JSAPI_plugin#createServiceString-Title-String-URL-String-Type-Boolean-Enabled-String-Icon
 // api 2 https://github.com/andoma/movian/blob/master/res/ecmascript/modules/movian/service.js#L27
@@ -74,6 +81,45 @@ new page.Route(PREFIX + ':list:(.*):(.*)', function(page, href, title) {
     page.metadata.icon = Plugin.path + 'logo.svg';
     page.metadata.logo = Plugin.path + 'logo.svg';
     console.log('открыта list');
+    //https://github.com/andoma/movian/blob/master/res/ecmascript/modules/movian/http.js#L75
+    //делаем запрос и возвращает https://movian.tv/projects/movian/wiki/JSAPI_http_response
+    //у нас тут будет запрос на url сайта + href
+    //https://w139.zona.plus/collections/new-year
+    console.log(href);
+    var resp = http.request(BASE_URL + href, {
+        'headers': {
+            'user-agent': UA,
+        },
+    });
+    //https://movian.tv/projects/movian/wiki/JSAPI_http_response#toString
+    //https://github.com/andoma/movian/blob/master/res/ecmascript/modules/movian/http.js#L48
+    //view-source:https://w139.zona.plus/collections/new-year
+    resp = resp.toString();
+    //код для консоли
+    // var items = document.getElementsByClassName('results')[0].children;
+    // [...items].forEach(function(i) {
+    //     var href = i.getElementByTagName('a')[0].attributes.getNamedItem('href').value;
+    //     var title = i.getElementByTagName('a')[0].attributes.getNamedItem('title').value;
+    //     var img = i.getElementByTagName('meta')[0].attributes.getNamedItem('content').value;
+    //     console.log(' title:' + title + '\n href:' + href + '\n img: ' + img);
+    // })
+
+    //parse a DOM
+    var dom = html.parse(resp);
+    document = dom.root;
+    var items = document.getElementByClassName('results')[0].children;
+    items.forEach(function(i) {
+        if (i.children.length) {
+            var href = i.getElementByTagName('a')[0].attributes.getNamedItem('href').value;
+            var title = i.getElementByTagName('a')[0].attributes.getNamedItem('title').value;
+            var img = i.getElementByTagName('meta')[0].attributes.getNamedItem('content').value;
+            // console.log(' title:' + title + '\n href:' + href + '\n img: ' + img);
+            page.appendItem(PREFIX + ':moviepage:' + href, 'video', {
+                title: title,
+                icon: img,
+            });
+        }
+    });
     page.loading = false;
 });
 // 3. «moviePage» страница с «контентом» на которую мы попадаем с листинга
